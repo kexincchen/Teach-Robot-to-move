08352bd7908253198a8e13e9ba53f5bf79d83ff1
Kaisheng Su <kaishengs@student.unimelb.edu.au>
2023-08-28 20:15:07 +1000

    Refactor the backend

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import config
 import time
 from exts import mongo
 from blueprints.auth import bp as auth_bp
+from blueprints.processing import bp as processing_bp
 from flask import Flask, request, jsonify, render_template, session, g, send_from_directory
 from bson import ObjectId
 
@@ -13,77 +14,17 @@ app.config.from_object(config)
 mongo.init_app(app)
 
 app.register_blueprint(auth_bp)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+app.register_blueprint(processing_bp)
+openai.api_key = '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'
 
 
-def generate_output(input_text):
-    # Use GPT-3.5 to generate the improved output
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0.8,
-        max_tokens=2000,
-        messages=[
-            {"role": "system", "content": f"Hello, world!"},
-            {"role": "user", "content": input_text}
-        ]
-    )
-    print(response.choices[0].message["content"])
-    return response.choices[0].message["content"]
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/speech-to-text', methods=['POST'])
-def stt():
-    print('[backend] speech-to-text')
-    if 'file' not in request.files:
-        print('[backend] audio not in request.files')
-        print(request.files['file'])
-        return jsonify({'error': 'No file inside the request'})
-    file = request.files['file']
-    if file.filename == '':
-        print('[backend] No selected file')
-        return jsonify({'error': 'No selected file'})
-
-    start_time = time.time()
-    file.save("uploaded_audio.mp3")
-    audiofile = open("uploaded_audio.mp3", "rb")
-    transcript = openai.Audio.translate("whisper-1", audiofile)
-    print(transcript['text'])
-    end_time = time.time()
-    stt_time = round(end_time-start_time, 2)
-    print("time is " + str(stt_time))
-    return jsonify({'textOutput': transcript['text'], 'time': stt_time})
-
-
-@app.route('/command', methods=['POST'])
-def command():
-    command_name = request.form['Command']
-    robot_command = mongo.db.Command.find_one({'name': command_name})
-    print(robot_command['command'])
-    if robot_command is None:
-        return jsonify({'robot_command': 'report_not_exist'})
-    else:
-        return jsonify({'robot_command': robot_command['command']})
-
-
-@app.route('/generate', methods=['POST'])
-def generate():
-    try:
-        data = request.json
-        input_text = data['input_text']
-
-        if not input_text:
-            return jsonify({"error": "Please provide input text, style, and platform."}), 400
-
-        output_text = generate_output(input_text)
-        return jsonify({"output_text": output_text}), 200
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/static/scripts/<path:filename>')
