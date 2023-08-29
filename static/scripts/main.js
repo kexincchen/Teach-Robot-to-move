cd2a2094c5347405790ec8a0b7b3b728d0e9aed8
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2023-08-29 19:00:13 +1000

    enabled to switch from nativejs to whisper

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,29 @@
+// Global variables
+let selectedModel = 'nativejs';  // Default model
+
+// Switch the model when the user changes the selection
+document.getElementById('modelSelect').addEventListener('change', function() {
+    selectedModel = this.value;
+    if (selectedModel === 'whisper') {
+        console.log("switched to whisper");
+        // document.getElementById('hints').removeEventListener("click");
+        initMediaRecorder();
+    } else if (selectedModel === 'nativejs') {
+        console.log("switched to nativejs");
+    }
+});
+
+// Common method to start recognition
+function startRecognition() {
+    if (selectedModel === 'whisper') {
+        // handleHintsClick(recognition);
+        
+    } else if (selectedModel === 'nativejs') {
+        document.getElementById('hints').click(); // trigger native JS click event
+        
+    }
+}
+
 // Initialize Speech Recognition
 function initSpeechRecognition() {
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -12,10 +38,12 @@ function initSpeechRecognition() {
     return recognition;
 }
 
-// Handle hints click
-function handleHintsClick(recognition) {
-    recognition.start();
+// Handle start click
+function handleStartClick(recognition) {
     console.log('Ready to receive a command.');
+    recognition.start();
+
+    
 }
 
 // Handle recognition results
@@ -51,16 +79,83 @@ async function handleFormSubmit(form, outputDiv) {
 // Main code
 const recognition = initSpeechRecognition();
 const diagnostic = document.querySelector('.output');
-const hints = document.querySelector('#hints');
+const start = document.querySelector('#start-recording');
 const form = document.getElementById('user-form');
+const stopBtn = document.getElementById('stop');
 const outputDiv = document.getElementById('text-output');
 
 // Set up event listeners
-hints.onclick = () => handleHintsClick(recognition);
+start.addEventListener("click", () => {
+    console.log("Start clicking...");
+    handleStartClick(recognition);
+});
 recognition.onresult = (event) => handleRecognitionResult(event, diagnostic);
-recognition.onspeechend = () => recognition.stop();
+// recognition.onspeechend = () => recognition.stop();
 recognition.onerror = (event) => handleRecognitionError(event, diagnostic);
+stopBtn.addEventListener("click", () => recognition.stop());
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     await handleFormSubmit(form, outputDiv);
 });
+
+
+function initMediaRecorder() {
+    console.log("whisper is ready");
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        console.log("getUserMedia supported.");
+        
+        return navigator.mediaDevices
+          .getUserMedia({ audio: true })
+          .then(function (stream) {
+            const mediaRecorder = new MediaRecorder(stream);
+            let chunks = [];
+
+            // Start recording
+            document.getElementById('hints').onclick = function() {
+                if (selectedModel === 'nativejs') {
+                    mediaRecorder.start();
+                    console.log(mediaRecorder.state);
+                    console.log("recorder started");
+                    document.getElementById('hints').disabled = true;
+                    document.getElementById('stop').disabled = false;
+                }
+            };
+            
+            // On data available
+            mediaRecorder.ondataavailable = function (e) {
+                chunks.push(e.data);
+            };
+
+            // Stop recording
+            document.getElementById('stop').onclick = function() {
+                if (selectedModel === 'nativejs') {
+                    mediaRecorder.stop();
+                    console.log(mediaRecorder.state);
+                    console.log("recorder stopped");
+                    document.getElementById('hints').disabled = false;
+                    document.getElementById('stop').disabled = true;
+                }
+            };
+
+            // On stop
+            mediaRecorder.onstop = function() {
+                console.log("recorder stopped");
+                const audioBlob = new Blob(chunks, { type: "audio/wav" });
+                chunks = [];
+                const audioUrl = URL.createObjectURL(audioBlob);
+                document.getElementById('downloadLink').href = audioUrl;
+                document.getElementById('downloadLink').style.display = 'block';
+                uploadAudio(audioBlob);
+            };
+
+            return mediaRecorder;
+          })
+          .catch(function (err) {
+            console.log("The following getUserMedia error occurred: " + err);
+            return null;
+          });
+    } else {
+        console.log("getUserMedia not supported on your browser!");
+        return null;
+    }
+}
--- static/scripts/selectModel.js
+++ static/scripts/selectModel.js
@@ -1,28 +1,11 @@
 function switchScript() {
     const modelSelect = document.getElementById("modelSelect").value;
-    const head = document.getElementsByTagName("head")[0];
-    let newScript;
-
-    // Remove existing script, if any
-    const existingScript = document.getElementById("dynamic-script");
-    if(existingScript) {
-        head.removeChild(existingScript);
-    }
 
     if (modelSelect === "whisper") {
-        newScript = "{{ url_for('static', filename='scripts/speechRecognition.js') }}";
         console.log("Change to whiser");
     } else if (modelSelect === "nativejs") {
-        newScript = "{{ url_for('static', filename='scripts/mediaRecorder.js') }}";
         console.log("Change to nativejs");
     }
 
-    // Create new script element
-    let scriptElement = document.createElement("script");
-    scriptElement.type = "text/javascript";
-    scriptElement.src = newScript;
-    scriptElement.id = "dynamic-script";
-    
-    // Append new script element to <head>
-    head.appendChild(scriptElement);
 }
+
