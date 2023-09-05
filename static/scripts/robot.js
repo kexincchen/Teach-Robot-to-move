c8460b142da14176f21dedf9c944a1ccea73c317
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2023-09-05 13:37:43 +1000

    added new model and can extract its animation

--- static/scripts/robot.js
+++ static/scripts/robot.js
@@ -3,7 +3,6 @@ import * as THREE from 'three';
 
 import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 let container, stats, clock, gui, mixer, actions, activeAction, previousAction;
@@ -58,7 +57,7 @@ function init() {
         model = gltf.scene;
         scene.add( model );
 
-        // createGUI( model, gltf.animations );
+        createGUI( model, gltf.animations );
 
     }, undefined, function ( e ) {
 
@@ -79,99 +78,57 @@ function init() {
 
 }
 
-function createGUI( model, animations ) {
-
-    const states = [ 'Idle', 'Walking', 'Running', 'Dance', 'Death', 'Sitting', 'Standing' ];
-    const emotes = [ 'Jump', 'Yes', 'No', 'Wave', 'Punch', 'ThumbsUp' ];
-
+function createGUI(model, animations) {
     gui = new GUI();
-
-    mixer = new THREE.AnimationMixer( model );
-
+    mixer = new THREE.AnimationMixer(model);
     actions = {};
 
-    for ( let i = 0; i < animations.length; i ++ ) {
+    addNewAnimations(mixer, animations);
 
-        const clip = animations[ i ];
-        const action = mixer.clipAction( clip );
-        actions[ clip.name ] = action;
+    getNewAnimations("static/models/XBot.glb", (otherModelAnimations, otherMixer) => {
+        addNewAnimations(otherMixer, otherModelAnimations);
+        createStatesGUI(); // Re-initialize states GUI after adding new animations.
+    });
 
-        if ( emotes.indexOf( clip.name ) >= 0 || states.indexOf( clip.name ) >= 4 ) {
-
-            action.clampWhenFinished = true;
-            action.loop = THREE.LoopOnce;
-
+    function createStatesGUI() {
+        for (var i = 0; i < gui.folders.length; i++) {
+            console.log(gui.folders[i]._title);
+            if (gui.folders[i]._title == "States") {
+                gui.remove(gui.folders[i]);
+            }
         }
 
+        const statesFolder = gui.addFolder('States');
+        const clipCtrl = statesFolder.add(api, 'state').options(Object.keys(actions));
+        
+        clipCtrl.onChange(function() {
+            fadeToAction(api.state, 0.5);
+        });
+
+        statesFolder.open();
     }
 
-    // states
-
-    const statesFolder = gui.addFolder( 'States' );
-
-    const clipCtrl = statesFolder.add( api, 'state' ).options( states );
-
-    clipCtrl.onChange( function () {
-
-        fadeToAction( api.state, 0.5 );
-
-    } );
-
-    statesFolder.open();
-
-    // emotes
-
-    const emoteFolder = gui.addFolder( 'Emotes' );
-
-    function createEmoteCallback( name ) {
-
-        api[ name ] = function () {
-
-            fadeToAction( name, 0.2 );
-
-            mixer.addEventListener( 'finished', restoreState );
-
-        };
-
-        emoteFolder.add( api, name );
-
-    }
+    createStatesGUI();
 
     function restoreState() {
-
-        mixer.removeEventListener( 'finished', restoreState );
-
-        fadeToAction( api.state, 0.2 );
-
+        mixer.removeEventListener('finished', restoreState);
+        fadeToAction(api.state, 0.2);
     }
 
-    for ( let i = 0; i < emotes.length; i ++ ) {
+    face = model.getObjectByName('Head_4');
+    const expressions = Object.keys(face.morphTargetDictionary);
+    const expressionFolder = gui.addFolder('Expressions');
 
-        createEmoteCallback( emotes[ i ] );
-
+    for (let i = 0; i < expressions.length; i++) {
+        expressionFolder.add(face.morphTargetInfluences, i, 0, 1, 0.01).name(expressions[i]);
     }
 
-    emoteFolder.open();
-
-    // expressions
-
-    face = model.getObjectByName( 'Head_4' );
-
-    const expressions = Object.keys( face.morphTargetDictionary );
-    const expressionFolder = gui.addFolder( 'Expressions' );
-
-    for ( let i = 0; i < expressions.length; i ++ ) {
-
-        expressionFolder.add( face.morphTargetInfluences, i, 0, 1, 0.01 ).name( expressions[ i ] );
-
-    }
-
-    activeAction = actions[ 'Walking' ];
+    activeAction = actions['Walking'];
     activeAction.play();
 
     expressionFolder.open();
-
 }
+
 
 function fadeToAction( name, duration ) {
 
@@ -217,3 +174,44 @@ function animate() {
     stats.update();
 
 }
+
+function getNewAnimations(modelURL, callback) {
+    const loader = new GLTFLoader();
+    loader.load(
+        modelURL,
+        (gltf) => {
+            // Create a new mixer for the imported model's animations.
+            const newMixer = new THREE.AnimationMixer(gltf.scene);
+            // Add animations to this new mixer.
+            addNewAnimations(newMixer, gltf.animations);
+            // Call the callback function with the loaded animations.
+            callback(gltf.animations, newMixer);
+        },
+        undefined, // onProgress callback, can be undefined if not needed.
+        (error) => {
+            console.error("An error occurred while loading the model:", error);
+        }
+    );
+}
+
+
+function addNewAnimations(mixer, animations) {
+
+
+    // Combine animations from the current model and the other model
+    // const combinedAnimations = animations.concat(otherModelAnimations);
+
+    for (let i = 0; i < animations.length; i++) {
+        const clip = animations[i].clone();
+        const action = mixer.clipAction(clip);
+        actions[clip.name] = action;
+        console.log("Add new action:", clip.name);
+
+        // Uncomment and modify as necessary for special loop or play conditions
+        // if (emotes.indexOf(clip.name) >= 0 || states.indexOf(clip.name) >= 4) {
+        //     action.clampWhenFinished = true;
+        //     action.loop = THREE.LoopOnce;
+        // }
+    }
+}
+
