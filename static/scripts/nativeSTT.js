06212a1242a9bf642ee75f56656258d77cb039d8
Charles_Ping <pzh1760473545@gmail.com>
2023-09-10 18:26:53 +1000

    Merge the specific files from clare-robotmove

--- static/scripts/nativeSTT.js
+++ static/scripts/nativeSTT.js
@@ -1,5 +1,59 @@
 //  ========= Speech recognition ================
 // From: https://github.com/mdn/dom-examples/blob/main/web-speech-api/speech-color-changer/script.js
+
+function initSpeechRecognition() {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    window.SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+    const recognition = new window.SpeechRecognition();
+    recognition.continuous = false;
+    recognition.lang = 'en-US';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    return recognition;
+}
+
+// // Handle start click
+// function handleStartClick(recognition) {
+//     console.log('Ready to receive a command.');
+//     recognition.start();
+// }
+
+// Handle recognition results
+function handleRecognitionResult(event, diagnostic) {
+    const result = event.results[0][0].transcript;
+    console.log(result);
+    diagnostic.value = result;
+}
+
+// Handle recognition errors
+function handleRecognitionError(event, diagnostic) {
+    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
+
+// Trigger Native JS model
+export function setNativejs(){
+    const recognition = initSpeechRecognition();
+    const diagnostic = document.querySelector('.output');
+    const start = document.querySelector('#start-recording');
+    const stopBtn = document.getElementById('stop');
+    console.log("Start clicking...");
+    stopBtn.style.display = "inline";
+    start.style.display = "none";
+    recognition.start();
+    recognition.onresult = (event) => handleRecognitionResult(event, diagnostic);
+    recognition.onerror = (event) => handleRecognitionError(event, diagnostic);
+    stopBtn.addEventListener("click", () => {
+        stopBtn.style.display = "none";
+        start.style.display = "inline";
+        recognition.stop();
+    });
+}
+
+// End of moudle.
+//==========================================divider==========================================
+
 function nativejsSTT() {
     var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 
