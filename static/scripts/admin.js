2a25ae0ba2016a48282da33461ccb43c08ee69d2
Charles_Ping <pzh1760473545@gmail.com>
2023-09-11 01:15:10 +1000

    Now can add and delete db

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -1,8 +1,8 @@
 // Fetch commands from the server when the page loads
 window.onload = function() {
     console.log("start admin");
-    fetch('/all_commands', {
-        method: 'GET'
+    fetch('/admin/all_commands', {
+        method: 'POST'
     })
     .then(response => response.json())
     .then(data => {
@@ -19,12 +19,31 @@ window.onload = function() {
     
 };
 
-document.getElementById('addCommandForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-    console.log("add command");
-    const commandName = document.getElementById('commandName').value;
+// document.getElementById('addCommandForm').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent the form from submitting the traditional way
+//     console.log("add command");
+//     const commandName = document.getElementById('commandName').value;
 
-    fetch('/add_command', {
+//     fetch('/admin/add_command', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ name: commandName })
+//     })
+//     .then(response => response.text())
+//     .then(result => {
+//         console.log('Command added:', result);
+//         // Optionally, you can refresh the list of commands or give some feedback to the user
+//     })
+//     .catch(error => {
+//         console.error('Error adding command:', error);
+//     });
+// });
+
+
+function addCommand(commandName) {
+    fetch('/admin/add_command', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -39,4 +58,34 @@ document.getElementById('addCommandForm').addEventListener('submit', function(ev
     .catch(error => {
         console.error('Error adding command:', error);
     });
-});
+}
+
+function deleteCommandByName(commandName) {
+    fetch('/admin/delete_command', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: commandName })
+    })
+    .then(response => response.text())
+    .then(result => {
+        console.log('Command deleted:', result);
+        // Optionally, refresh the list of commands or give feedback to the user
+    })
+    .catch(error => {
+        console.error('Error deleting command:', error);
+    });
+}
+
+
+function performSelectedOperation() {
+    const commandName = document.getElementById('commandName').value;
+    const selectedOperation = document.getElementById('operationSelect').value;
+
+    if (selectedOperation === "add") {
+        addCommand(commandName);
+    } else if (selectedOperation === "delete") {
+        deleteCommandByName(commandName);
+    }
+}
