205616074bcb34ce380546c40170f8c2695f36b5
Charles_Ping <pzh1760473545@gmail.com>
2023-09-20 11:11:57 +1000

    Make the test  of main page

--- test_app.py
+++ test_app.py
@@ -1,12 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return "Welcome to my Flask app!"
+from app import app
 
 def test_index():
     client = app.test_client()
     response = client.get('/')
     
     assert response.status_code == 200
-    assert b"Welcome to my Flask app!" in response.data
+    # Optionally, you can add more assertions based on expected response data.
