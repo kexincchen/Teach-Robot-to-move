0f406a032b7b022acb2b7a13680d9e197fc1f835
Charles_Ping <pzh1760473545@gmail.com>
2023-09-20 11:07:17 +1000

    Make the test app

--- test_app.py
+++ test_app.py
@@ -3,3 +3,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Welcome to my Flask app!"
+
+def test_index():
+    client = app.test_client()
+    response = client.get('/')
+    
+    assert response.status_code == 200
+    assert b"Welcome to my Flask app!" in response.data
